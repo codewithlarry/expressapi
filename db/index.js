7543c278605d83ecfd247006117e73aa2efdedc3
codewithlarry <larry@codewithlarry.com>
2021-06-01 11:06:58 -0700

    update code for sequelize

--- db/index.js
+++ db/index.js
@@ -1,8 +1,7 @@
 const {Model, DataTypes} = require("sequelize");
-const dataSource = require('../dataSource');
+const dataSource = require("./dataSource");
 
 class Products extends Model{}
-
 Products.init({
     id:{
         type:DataTypes.INTEGER,primaryKey:true,autoIncrement:true
@@ -15,9 +14,21 @@ Products.init({
     },
     userId:{
         type:DataTypes.INTEGER, allowNull:false
-    }
+    },
+    memo:{
+        type:DataTypes.STRING, allowNull:true
+    }   
 },{
     sequelize:dataSource,
     tableName: "products"
 });
-module.exports = Products;
+
+(async () => {
+    try{
+        await dataSource.sync( {alter: true} );
+    }catch(err){
+        console.error(err);
+    }finally{
+        await dataSource.close();
+    }
+})();
